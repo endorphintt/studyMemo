5543c73983b4f21c44a954ac46d24e4d301096d7
endorphintt <bamko@ukr.net>
2023-09-01 20:19:46 +0200

    starting back 2.0

--- backend/routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ImageController = require('../controllers/images');
-
-router.post('/add', ImageController.createImage);
-router.get('/', ImageController.getAllImages)
-
-module.exports = router;
